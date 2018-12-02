853c387cb483751832dc9b48e8eff666e6621cab
wan03 <38962452+wan03@users.noreply.github.com>
2018-12-01 23:13:43 -0500

    Started setting up Passport, still a long way to go

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var passport = require("passport");
+var Strategy = require("passport-local").Strategy;
 var db = require("./models");
-
+var flash = require("connect-flash");
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+require("./config/passport")(passport);
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use(require("morgan")("combined"));
+app.use(require("cookie-parser")());
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "vase red",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(
+  session({
+    secret: "vidyapathaisalwaysrunning",
+    resave: true,
+    saveUninitialized: true
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
 // Handlebars
 app.engine(
   "handlebars",
@@ -24,6 +46,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/loginRoutes")(app, passport);
 
 var syncOptions = { force: false };
 
